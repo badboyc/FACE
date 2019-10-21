5a9c597ed4ef5edf33098739fc6bf7d3d4bc21d0
wym1996 <1136965437@qq.com>
2019-10-21 17:56:19 +0800

    更改

--- ant-design-vue-jeecg/src/components/layouts/UserLayout.vue
+++ ant-design-vue-jeecg/src/components/layouts/UserLayout.vue
@@ -5,11 +5,11 @@
         <div class="header">
           <a href="/">
             <img src="~@/assets/logo.svg" class="logo" alt="logo">
-            <span class="title">Jeecg Boot</span>
+            <span class="title">机载软件可移植单元</span>
           </a>
         </div>
         <div class="desc">
-          Jeecg Boot 是中国最具影响力的 企业级 快速开发平台
+
         </div>
       </div>
 
@@ -22,7 +22,7 @@
           <a href="https://github.com/zhangdaiscott/jeecg-boot" target="_blank">条款</a>
         </div>
         <div class="copyright">
-          Copyright &copy; 2019 <a href="http://www.jeecg.org" target="_blank">JEECG开源社区</a> 出品
+          Copyright &copy; 2019 <a href="http://www.jeecg.org" target="_blank">306实验室</a> 出品
         </div>
       </div>
     </div>
