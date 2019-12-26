fa1d4905c760d600e4067e8d3e0a76f270bc6847
wym <1136965437@qq.com>
2019-12-26 20:08:30 +0800

    图片添加

--- ant-design-vue-jeecg/src/views/picture/modules/PictureModal.vue
+++ ant-design-vue-jeecg/src/views/picture/modules/PictureModal.vue
@@ -10,12 +10,24 @@
     
     <a-spin :spinning="confirmLoading">
       <a-form :form="form">
-      
         <a-form-item
           :labelCol="labelCol"
           :wrapperCol="wrapperCol"
           label="url">
-          <a-input placeholder="请输入url" v-decorator="['url', {}]" />
+          <!--<a-input placeholder="请输入url" v-decorator="['url', {}]" />-->
+          <a-upload
+            name="file"
+            :multiple="false"
+            :action="uploadAction"
+            :headers="headers"
+            :data="{'isup':1,'bizPath':bizPath}"
+            :fileList="fileList"
+            :beforeUpload="beforeUpload"
+            @change="handleChange">
+            <a-button>
+              <a-icon type="upload" />{{ text }}
+            </a-button>
+          </a-upload>
         </a-form-item>
         <a-form-item
           :labelCol="labelCol"
@@ -33,6 +45,28 @@
   import { httpAction } from '@/api/manage'
   import pick from 'lodash.pick'
   import moment from "moment"
+  /******************************************************************************************/
+  import Vue from 'vue'
+  import { ACCESS_TOKEN } from "@/store/mutation-types"
+
+  const FILE_TYPE_ALL = "all"
+  const FILE_TYPE_IMG = "image"
+  const FILE_TYPE_TXT = "file"
+  const uidGenerator=()=>{
+      return '-'+parseInt(Math.random()*10000+1,10);
+  }
+  const getFileName=(path)=>{
+      if(path.lastIndexOf("\\")>=0){
+          let reg=new RegExp("\\\\","g");
+          path = path.replace(reg,"/");
+      }
+      return path.substring(path.lastIndexOf("/")+1);
+  }
+  var urlPath = window.document.location.href;
+  var docPath = window.document.location.pathname;
+  var index = urlPath.indexOf(docPath);
+  var serverPath = urlPath.substring(0, index);
+  /******************************************************************************************/
 
   export default {
     name: "PictureModal",
@@ -58,13 +92,63 @@
           add: "/picture/picture/add",
           edit: "/picture/picture/edit",
         },
+          /******************************************************************************************/
+          uploadAction:serverPath+"/jeecg-boot"+"/sys/common/upload",
+          //urlDownload:window._CONFIG['domianURL'] + "/sys/common/download/",
+          urlDownload:serverPath +"/jeecg-boot"+ "/sys/download/download/",
+          headers:{},
+          fileList: [],
+          pictureUrl:''
+
+          /******************************************************************************************/
       }
     },
-    created () {
+      /******************************************************************************************/
+      props:{
+          text:{
+              type:String,
+              required:false,
+              default:"点击上传"
+          },
+          fileType:{
+              type:String,
+              required:false,
+              default:FILE_TYPE_ALL
+          },
+          /*这个属性用于控制文件上传的业务路径*/
+          bizPath:{
+              type:String,
+              required:false,
+              default:"temp"
+          },
+          value:{
+              type:String,
+              required:false
+          },
+          //此属性被废弃了
+          triggerChange:{
+              type: Boolean,
+              required: false,
+              default: false
+          },
+      },
+      watch:{
+          value(val){
+              this.initFileList(val)
+          }
+      },
+
+      /******************************************************************************************/
+
+
+      created () {
+          const token = Vue.ls.get(ACCESS_TOKEN);
+          this.headers = {"X-Access-Token":token}
     },
     methods: {
       add () {
         this.edit({});
+        this.pictureUrl=''
       },
       edit (record) {
         this.form.resetFields();
@@ -76,6 +160,95 @@
         });
 
       },
+
+        initFileList(paths){
+            if(!paths || paths.length==0){
+                this.fileList = [];
+                return;
+            }
+            let fileList = [];
+            let arr = paths.split(",")
+            for(var a=0;a<arr.length;a++){
+                fileList.push({
+                    uid:uidGenerator(),
+                    name:getFileName(arr[a]),
+                    status: 'done',
+                    url: this.urlDownload+arr[a],
+                    response:{
+                        status:"history",
+                        message:arr[a]
+                    }
+                })
+            }
+            this.fileList = fileList
+        },
+
+        handlePathChange(){
+            let uploadFiles = this.fileList
+            let path = ''
+            if(!uploadFiles || uploadFiles.length==0){
+                path = ''
+            }
+            let arr = [];
+
+            for(var a=0;a<uploadFiles.length;a++){
+                arr.push(uploadFiles[a].response.message)
+            }
+            if(arr.length>0){
+                path = arr.join(",")
+            }
+            this.$emit('change', path);
+        },
+
+        beforeUpload(file){
+            var fileType = file.type;
+            if(fileType===FILE_TYPE_IMG){
+                if(fileType.indexOf('image')<0){
+                    this.$message.warning('请上传图片');
+                    return false;
+                }
+            }else if(fileType===FILE_TYPE_TXT){
+                if(fileType.indexOf('image')>=0){
+                    this.$message.warning('请上传文件');
+                    return false;
+                }
+            }
+            //TODO 扩展功能验证文件大小
+            return true
+        },
+
+        handleChange(info) {
+            console.log("--文件列表改变--")
+            let fileList = info.fileList
+            fileList = fileList.slice(-1);
+            if(info.file.status==='done'){
+                if(info.file.response.success){
+                    fileList = fileList.map((file) => {
+                        if (file.response) {
+                            file.url = this.urlDownload + file.response.message;
+                            this.pictureUrl = file.url;
+                        }
+                        return file;
+                    });
+                }
+                this.$message.success(`${info.file.name} 上传成功!`);
+            }else if (info.file.status === 'error') {
+                this.$message.error(`${info.file.name} 上传失败.`);
+            }else if(info.file.status === 'removed'){
+                this.handleDelete(info.file)
+            }
+            this.fileList = fileList
+            if(info.file.status==='done' || info.file.status === 'removed'){
+                this.handlePathChange()
+            }
+        },
+
+        handleDelete(file){
+            //如有需要新增 删除逻辑
+            console.log(file)
+        },
+
+
       close () {
         this.$emit('close');
         this.visible = false;
@@ -96,6 +269,7 @@
                method = 'put';
             }
             let formData = Object.assign(this.model, values);
+            formData.url = this.pictureUrl;
             //时间格式化
             
             console.log(formData)
@@ -119,9 +293,11 @@
       handleCancel () {
         this.close()
       },
-
-
-    }
+    },
+      model: {
+          prop: 'value',
+          event: 'change'
+      }
   }
 </script>
 
